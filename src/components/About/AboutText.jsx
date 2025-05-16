03ef3467f89c5b636d9ae2358b01c84a78a92d90
kat-lev <kat@katleverton.com>
2025-05-16 14:58:07 +0200

    fix: Rearrange About page

--- src/components/About/AboutText.jsx
+++ src/components/About/AboutText.jsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import styles from "./../About/AboutText.module.css";
 
 function AboutText() {   
 const { t, i18n } = useTranslation();  
 
 return(
-    <div> 
+    <div className={styles.aboutText}> 
       <h2>{t('about.title')}</h2>
       <p>{t('about.description')}</p>
     </div>
