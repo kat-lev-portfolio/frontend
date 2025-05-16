7bbf2ee5417c2cbdbb85c6f19ac60470f3fc1777
kat-lev <kat@katleverton.com>
2025-05-16 12:33:32 +0200

    Update Language file

--- src/components/Projects/ProjectText.jsx
+++ src/components/Projects/ProjectText.jsx
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 function ProjectText() {
+const { t, i18n } = useTranslation();
+
   return (
     <div>
-        <h1>Project title</h1>
-        <p>Project text</p>
+      <h1>{t('project.title')}</h1>
+      <p>{t('project.description')}</p>
     </div>
   );
 }
